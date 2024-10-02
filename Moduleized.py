deddfff43658f2d49c3b8dd6ff77076f7c6dfde7
Justmore5mins <justmore5mins@gmail.com>
2024-10-02 20:22:29 +0800

    add

--- Moduleized.py
+++ Moduleized.py
@@ -1,4 +1,4 @@
 from detect import OpenVino,Detect
 
 #OpenVino("FRC_openvino_model/FRC.xml", device_name="CPU").stream(camera=0, gui=True)
-Detect("FRC.pt").stream(camera=0, cls=[3], gui=True)
+Detect("runs/detect/train/weights/best.pt").stream(camera=0, gui=True)
